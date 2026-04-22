a8cc4756b42172fee89e90d6be6cf86f0e85cfa8
Md. Sazzadul Islam <sazzadul.islam@bkash.com>
2026-04-22 16:02:08 +0600

    fix: FOR-0 type and vite config

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,18 +1,11 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    tsconfigPaths: true,
   },
 });
